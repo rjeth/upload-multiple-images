b39c70c0ddd70925e0b098f680718e5d9f960d2e
ジェスロ・ジョロラン <40770876+rjeth@users.noreply.github.com>
2021-11-12 21:14:58 +0800

    Upload images

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Image;
+use Illuminate\Http\Request;
+
+class ImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('upload.create');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('upload.create');
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'images' => 'required',
+          ]);
+  
+          if ($request->hasfile('images')) {
+              $images = $request->file('images');
+  
+              foreach($images as $image) {
+                  $name = $image->getClientOriginalName();
+                  $path = $image->storeAs('uploads', $name, 'public');
+  
+                  Image::create([
+                      'name' => $name,
+                      'path' => '/storage/'.$path
+                    ]);
+              }
+           }
+  
+          return back()->with('success', 'Images uploaded successfully');
+      
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Image  $image
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Image $image)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Image  $image
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Image $image)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Image  $image
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Image $image)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Image  $image
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Image $image)
+    {
+        //
+    }
+}
